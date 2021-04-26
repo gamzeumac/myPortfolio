6a7cb12d40a19d1ae4fd603ad71d0aec0ae97e5e
gamzeumac <umacgamze49@gmail.com>
2021-04-26 18:03:43 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
+import React from 'react';
+import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
+import NavBar from './components/NavBar';
+import Header from './components/Header';
+import About from './components/About';
+import Project from './components/Project';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
 
+    <Router>
+      <div className="App">
+        <NavBar/>
+        <Header />
 
-    </div>
+        <Switch>
+          <Route path="/Project">
+            <Project />
+          </Route>
+          <Route path="/About">
+            <About />
+          </Route>
+          <Route path="/Skills">
+            <Skills />
+          </Route>
+          <Route path="/Contact">
+            <Contact />
+          </Route>
+
+          <Route exact path="/">
+            <Header />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
