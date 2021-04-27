07dc1aac83a7355bd47c534062b04417997eb063
gamzeumac <umacgamze49@gmail.com>
2021-04-27 18:57:13 +0200

    projekt

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,25 +5,22 @@ import llogo from '../images/llogo.png';
 
 
 const NavBar = () => {
-    return (
-<Navbar bg="dark" expand="lg">
-  <Navbar.Brand href="#home"><img className="logo" src={llogo} alt="logo..." /></Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#About">About</Nav.Link>
-      <Nav.Link href="#Skills">Skills</Nav.Link>
-      <Nav.Link href="#Project">Project</Nav.Link>
-      <Nav.Link href="#Contact">Contact</Nav.Link>
+  return (
+    <Navbar bg="dark" expand="lg">
+      <Navbar.Brand href="#home"><img className="logo" src={llogo} alt="logo..." /></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#About">About</Nav.Link>
+          <Nav.Link href="#Skills">Skills</Nav.Link>
+          <Nav.Link href="#Project">Project</Nav.Link>
+          <Nav.Link href="#Contact">Contact</Nav.Link>
 
-    </Nav>
+        </Nav>
 
-  </Navbar.Collapse>
-</Navbar>
-    
-    )
-
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
-
 export default NavBar;
