91dde1b6e2e5e21850d492562e6160f5b77613d1
gamzeumac <umacgamze49@gmail.com>
2021-04-28 00:06:55 +0200

    maps

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,41 +1,36 @@
 import React from 'react'
-import fruits from "../images/fruits.png";
-import  "../data.json";
+import data from "../data.json";
 import "react-popupbox/dist/react-popupbox.css";
+import fruits from "../images/fruits.png";
+import perse from "../images/perse.png";
+
+
 
 const Project = () => {
-    let projects = [
-      {
-        name:'',
-        url:'',
-        type:'',
-        programmingLanguages: ['React','Bootstrap','Scss']
-      }
-    ]
+
     return (
         <div id="portfolio" className="portfolio-wrapper">
       <div className="container">
-        <h1 className="text-uppercase text-center py-5">portfolio</h1>
+        <h1 className="text-uppercase text-center py-5">Portfolio</h1>
  
- {projects.map((item,index) => {
+    {data.map((item,index) => {
          return <div className="image-box-wrapper row justify-content-center">
             <div>Name:{item.name}</div>
             <br/>
             <div>Project Type: {item.type}</div>
           <div className="portfolio-image-box">
-         <a href={item.url} target="_blank" rel="noreferrer">   <img className="portfolio-image" src={fruits} alt="Fruits Project..." /></a>
-           
+         <a href={item.url} target="_blank" rel="noreferrer">   <img className="portfolio-image" src={fruits} alt="Fruits Project..." /></a>           
            <div>
              Programming Langaugeas <ul>
                {item.programmingLanguages.map((item,index)=>{
                  return <li>{item}</li>
                })}
              </ul>
-           </div>
-            
+           </div>            
           </div>
+
           </div>
- })}
+    })}
           </div>
          
           </div>
