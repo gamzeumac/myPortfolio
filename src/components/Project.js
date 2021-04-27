72505965e358f041dfaa88d37d6ee7ab4ee10da6
gamzeumac <umacgamze49@gmail.com>
2021-04-27 23:17:54 +0200

    data.json

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,51 +1,43 @@
 import React from 'react'
 import fruits from "../images/fruits.png";
-
-import { PopupboxManager, PopupboxContainer } from "react-popupbox";
+import  "../data.json";
 import "react-popupbox/dist/react-popupbox.css";
 
 const Project = () => {
-    const openPopupboxFruits = () => {
-        const content = (
-            <>
-            <img className="portfolio-image-popupbox"src={fruits} alt="Fruits Project..." />
-        <p></p>
-        <div className='links'>
-          <b className='demo'>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://gamzeumac.github.io/Fruitspage/", "_blank")}>https://gamzeumac.github.io/Fruitspage/</a>
-          <br />
-          <b className='demo'>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/gamzeumac/Fruitspage")}>https://github.com/gamzeumac/Fruitspage</a>
-        </div>
-      </>
-        )
-        PopupboxManager.open({ content })
-    }
-
-    const popupboxConfigFruits = {
-        titleBar: {
-          enable: true,
-          text: "Fruits Project....."
-        },
-        fadeIn: true,
-        fadeInSpeed: 500
+    let projects = [
+      {
+        name:'',
+        url:'',
+        type:'',
+        programmingLanguages: ['React','Bootstrap','Scss']
       }
-    
-
-
-
+    ]
     return (
         <div id="portfolio" className="portfolio-wrapper">
       <div className="container">
         <h1 className="text-uppercase text-center py-5">portfolio</h1>
-        <div className="image-box-wrapper row justify-content-center">
-          {/* - */}
-          <div className="portfolio-image-box" onClick={openPopupboxFruits}>
-            <img className="portfolio-image" src={fruits} alt="Fruits Project..." />
-            <div className="overflow"></div>
+ 
+ {projects.map((item,index) => {
+         return <div className="image-box-wrapper row justify-content-center">
+            <div>Name:{item.name}</div>
+            <br/>
+            <div>Project Type: {item.type}</div>
+          <div className="portfolio-image-box">
+         <a href={item.url} target="_blank" rel="noreferrer">   <img className="portfolio-image" src={fruits} alt="Fruits Project..." /></a>
+           
+           <div>
+             Programming Langaugeas <ul>
+               {item.programmingLanguages.map((item,index)=>{
+                 return <li>{item}</li>
+               })}
+             </ul>
+           </div>
             
           </div>
           </div>
+ })}
           </div>
-          <PopupboxContainer {...popupboxConfigFruits} />
+         
           </div>
 
     )
