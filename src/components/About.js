b1532d1e631be1f77ca9761adfad4fa44c81a3f5
gamzeumac <umacgamze49@gmail.com>
2021-04-28 03:45:51 +0200

    update

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,11 @@ const About = () => {
     return (
         <div>
             
-            <Container>
+            <Container className="about" >
                 <Row >
                     <Col xs={12} md={6} lg={6}><img className="me" src={me} alt="me"/></Col>
                     <Col xs={12} md={6} lg={6}> 
-                    <h4> About Me</h4>
+                    <h1> About Me</h1>
                     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                     </Col>
                 </Row>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,40 +1,45 @@
 import React from 'react'
 import data from "../data.json";
 import "react-popupbox/dist/react-popupbox.css";
+import {Row, Col, Card, Button} from 'react-bootstrap'
 
 
 
 
 const Project = () => {
 
-    return (
-        <div id="portfolio" className="portfolio-wrapper">
-      <div className="container">
-        <h1 className="text-uppercase text-center py-5">Portfolio</h1>
+    return (<>
+    <h1 className="text-uppercase text-center py-5">Portfolio</h1>
+      <Row>
+        
+  
+        
  
     {data.map((item,index) => {
-         return <div className="image-box-wrapper row justify-content-center">
-            <div>Name:{item.name}</div>
-            <br/>
-            <div>Project Type: {item.type}</div>
-          <div className="portfolio-image-box">
-         <a href={item.url} target="_blank" rel="noreferrer"> <img className="portfolio-image" src={item.image} width="15rem" alt="" /></a>           
+         return <Col lg={3} className="mb-4 p-3">
+         <Card style={{ width: '20rem' , height:"15rem" }}>
+         <Card.Img variant="top" src="{item.image} " />
+         <Card.Body>
+           <Card.Title>{item.name} </Card.Title>
+           <Card.Text>
            <div>
              Programming Langaugeas <ul>
                {item.programmingLanguages.map((item,index)=>{
                  return <li>{item}</li>
                })}
              </ul>
-           </div>            
-          </div>
+           </div>     
+           </Card.Text>
+           <Button variant="secondary">{item.url}</Button>
+         </Card.Body>
+       </Card>
 
-          </div>
+       </Col> 
     })}
-          </div>
-         
-          </div>
 
-    )
+          </Row>
+
+   </> )
 }
 
 export default Project;
