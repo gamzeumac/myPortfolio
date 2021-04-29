22cb918e8a1e44bc4b56f5d78288ad02937e7f82
gamzeumac <umacgamze49@gmail.com>
2021-04-30 00:42:16 +0200

    bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,9 +12,9 @@ const NavBar = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#About">About</Nav.Link>
-          <Nav.Link href="#Skills">Skills</Nav.Link>
+          <Nav.Link href="#About">About</Nav.Link>          
           <Nav.Link href="#Project">Project</Nav.Link>
+          <Nav.Link href="#Skills">Skills</Nav.Link>
           <Nav.Link href="#Contact">Contact</Nav.Link>
 
         </Nav>
