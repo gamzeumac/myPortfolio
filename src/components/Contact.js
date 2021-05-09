44a043cbed1b28867f7b44d60514859da0332788
g <umacgamze49@gmail.com>
2021-05-09 15:44:49 +0200

    data.json

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
-import { Form, Button,DropdownButton, Dropdown } from 'react-bootstrap';
+import { Form, Button, DropdownButton, Dropdown } from 'react-bootstrap';
 
 const Contact = () => {
 
@@ -17,27 +17,27 @@ const Contact = () => {
 
   return (
     <div>
-      <Form as="Col" className="d-flex flex-column p-5  mx-5">
-        <Form.Group controlId="exampleForm.ControlInput1" onSubmit={sendEmail}>
+      <Form as="Col" className="d-flex flex-column p-5  mx-5" onSubmit={sendEmail}>
+        <Form.Group controlId="exampleForm.ControlInput" >
           <Form.Label>Name</Form.Label>
           <Form.Control type="text" placeholder="name" />
         </Form.Group>
 
-        <Form.Group controlId="exampleForm.ControlInput1">
+        <Form.Group controlId="exampleForm.ControlInput">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="name@example.com" />
         </Form.Group>
 
-        <Form.Group controlId="exampleForm.ControlSelect1">
+        <Form.Group controlId="exampleForm.ControlSelect">
           <Form.Label>Example multiple select</Form.Label>
           <DropdownButton id="dropdown-basic-button" title="Example multiple select">
-  <Dropdown.Item href="#/action-1">I offer job for you</Dropdown.Item>
-  <Dropdown.Item href="#/action-2">I need professional support for my project</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">Other</Dropdown.Item>
-</DropdownButton>
+            <Dropdown.Item href="#/action-1">I offer job for you</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">I need professional support for my project</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Other</Dropdown.Item>
+          </DropdownButton>
 
         </Form.Group>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Group controlId="exampleForm.ControlTextarea">
           <Form.Label>Example textarea</Form.Label>
           <Form.Control as="textarea" rows={3} />
         </Form.Group>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,85 +1,88 @@
 import React from 'react';
 import { Container, Row, Col, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import data from "../data.json";
 
 
 
 
 const Skills = () => {
-    const renderTooltip = props => (
-        <Tooltip {...props}>still learning</Tooltip>
-      );
+  const renderTooltip = props => (
+    <Tooltip {...props}>still learning</Tooltip>
+  );
 
-      const renderTooltip1 = props1 => (
-        <Tooltip {...props1}>Visual Studio Code</Tooltip>
-      );
-      const renderTooltip2 = props2 => (
-        <Tooltip {...props2}>Github</Tooltip>
-      );
-      const renderTooltip3 = props3 => (
-        <Tooltip {...props3}>Scrum</Tooltip>
-      );
-    return (
-        
-        <div>
+  const renderTooltip1 = props1 => (
+    <Tooltip {...props1}>Visual Studio Code</Tooltip>
+  );
+  const renderTooltip2 = props2 => (
+    <Tooltip {...props2}>Github</Tooltip>
+  );
+  const renderTooltip3 = props3 => (
+    <Tooltip {...props3}>Scrum</Tooltip>
+  );
+  return (
 
-            <h1  className="text-center"> My Skills</h1>
-            
-            <Container className="myskills" >
+    <div>
+
+      <h1 className="text-center"> My Skills</h1>
+
+      <Container className="myskills" >
 
 
-                <Row >
-                    <Col className="my-5 p-5" xs={12} md={6} lg={6}> <h4> Frontend</h4> <div className="icons">
-                    <div><img src="images/html.png" alt="" /></div> 
-                    <div><img src="images/css1.png" alt="" /></div> 
-                    <div><img src="images/bootstrap.png" alt=""/> </div>
-                    <div><img src="images/js.png" alt="" /> </div>
-                    <div><img src="images/sass.png" alt="" /> </div>
-                    <OverlayTrigger placement="top" overlay={renderTooltip}>
-                    <div><img src="images/nodejs.png" alt="" /> </div></OverlayTrigger>
-                    <OverlayTrigger placement="top" overlay={renderTooltip}>
-                    <div><img src="images/react.png" alt="" /> </div></OverlayTrigger>
-                    <OverlayTrigger placement="top" overlay={renderTooltip}> 
-                    <div ><img src="images/typescript.png" alt="" /> </div></OverlayTrigger>
-                    <OverlayTrigger placement="top" overlay={renderTooltip}>
-                     <div ><img src="images/vuejs.png" alt="" /> </div></OverlayTrigger>
-                    <OverlayTrigger placement="top" overlay={renderTooltip}>
-                    <div ><img src="images/docker.png" alt="" /></div></OverlayTrigger>
-        
-                     
+        <Row >
+          <Col className="my-5 p-5 d-flex flex-col" xs={12} md={6} lg={6}> <h4> Frontend</h4>
+            {data.skills.map((item, index) => {
 
-                    </div> 
-                    </Col>
-                    <Col className="my-5 p-5"  xs={12} md={6} lg={6}><h4>Language</h4>
-                    <div>Deutsch <ProgressBar striped variant="info" now={50} /></div>
-                    <div>Englisch <ProgressBar striped variant="warning" now={40} /></div>
-                    <div>Türkisch <ProgressBar striped variant="success" now={100} /></div>
-                    </Col>
+              return <div>
 
-                   
-                    <Col className="my-5 p-5" xs={12} md={6} lg={6}> <h4>Backend</h4> 
-                    <div className="mern"><img src="images/mern.png" alt=""/></div>
-                
-                    
-                    </Col>
-                    
-                    <Col className="others my-5 p-5" xs={12} md={6} lg={6}> <h4>Other</h4> 
-                    <OverlayTrigger placement="top" overlay={renderTooltip1}> 
-                    <div  ><img src="images/vsc.png" alt="" /> </div></OverlayTrigger>                    
-                    <OverlayTrigger placement="top" overlay={renderTooltip2}>
-                        <div ><img src="images/github1.png" alt="" /></div></OverlayTrigger> 
-                        <OverlayTrigger placement="top" overlay={renderTooltip3}>
-                            <div ><img src="images/agile.png" alt="" /> </div></OverlayTrigger> 
-                    </Col>
-               
-                
-                    
-                   
-                    </Row>
-                
-            </Container>
-        </div>
-    )
+                {item.learning ? <OverlayTrigger placement="top" overlay={renderTooltip}>
+                  <div ><img src={item.image} alt="" /></div></OverlayTrigger>
+                  : <div ><img src={item.image} alt="" /></div>}
+              </div>
+            })}
+          </Col>
+          <Col className="my-5 p-5" xs={12} md={6} lg={6}><h4>Language</h4>
+            {data.languages.map((item, index) => {
+
+              return <div>
+                <div>{item.name1}</div>
+
+                {item.learning ? <ProgressBar striped variant="info" now={50} />:false}
+                <div> {item.degree}</div>
+                </div>
+              
+            })}
+          </Col>
+          <Col className="my-5 p-5" xs={12} md={6} lg={6}> <h4>Backend</h4>
+            <div className="mern"><img src="images/mern.png" alt="" /></div>
+          </Col>
+
+          <Col className="others my-5 p-5" xs={12} md={6} lg={6}> <h4>Other</h4>
+          {data.other.map((item, index) => {
+            return <div>
+              <div><img src={item.image} alt=""/></div>
+              {item.learning ? <OverlayTrigger placement="top" overlay={item.tooltip} />:false}
+                <div> {item.tooltip}</div>
+              
+            </div>  
+
+            })}
+            <OverlayTrigger placement="top" overlay={renderTooltip1}>
+              <div  ><img src="images/vsc.png" alt="" /> </div></OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={renderTooltip2}>
+              <div ><img src="images/github1.png" alt="" /></div></OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={renderTooltip3}>
+              <div ><img src="images/agile.png" alt="" /> </div></OverlayTrigger>
+          </Col>
+
+
+
+
+        </Row>
+
+      </Container>
+    </div>
+  )
 }
 
 export default Skills
