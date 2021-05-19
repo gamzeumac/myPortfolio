6c3ce47c9dc257e25d37299d361dcdd7a9843737
g <umacgamze49@gmail.com>
2021-05-20 00:54:52 +0200

    contact us ist fertig

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,34 +1,38 @@
-import React, {useState,  useEffect } from 'react';
+import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 import { Form, Button, DropdownButton, Dropdown } from 'react-bootstrap';
+import sucsess from "../images/thankyou.jpg";
+import error from "../images/error.jpg";
 
 
 
 
 const Contact = () => {
   const [successMessage, setSuccessMessage] = useState("");
-  
+  const [errorMessage, seterrorMessage] = useState("");
+
   function sendEmail(e) {
     e.preventDefault();
-    emailjs.sendForm('serviceID', 'templateID', e.target, 'user_Gs6nx0Emi4Jrc3gFs28Vm')
-            .then(() => {
-               
-              
-              setSuccessMessage("Form sent successfully! I'll contact you as soon as possible.");
+    emailjs.sendForm('serviceID', 'templateID', e.target, 'user_Gs6nx0Emi4Jrc3gFs28Vm' )
+      .then(() => {
 
-            }, (error) => {
-                console.log(error.text);
-            });
-   }
+
+        setSuccessMessage(<img className="d-flex flex-column h-50 p-5 mx-5 thankyou" src={sucsess} alt="" 
+        /> );
+
+      }, (error) => {
+        seterrorMessage(<img className="d-flex flex-column h-50 p-5 mx-5 thankyou " src={error} alt="" />);
+      });
+  }
 
 
   return (
     <div>
       <h2 className="text-center">Contact Me</h2>
       <p className="text-center">Please fill out the form and describe you project needs and I'll contact you as soon as possible.</p>
-        <span className="success-message">{successMessage}</span>
+      <span className="success-message">{successMessage}</span>
       <form as="Col" className="d-flex flex-column h-50 p-5 mx-5 contact" onSubmit={sendEmail}>
-       <Form.Group controlId="exampleForm.ControlInput" >
+        <Form.Group controlId="exampleForm.ControlInput" >
           <Form.Label>Name</Form.Label>
           <Form.Control type="text" placeholder="name" />
         </Form.Group>
@@ -50,8 +54,8 @@ const Contact = () => {
         <Form.Group controlId="exampleForm.ControlTextarea">
           <Form.Label>Example textarea</Form.Label>
           <Form.Control as="textarea" rows={3} />
-        </Form.Group> 
-        
+        </Form.Group>
+
         <Button variant="info" type="submit" >
           Submit
   </Button>
